b6676b9470492f9cb6a2510d15f893df489796bf
Jin Wang (Inspur) <v-jiwag@microsoft.com>
2017-11-04 12:49:07 +0800

    add xunit

---
+++ core/test/AspectCore.Tests/DynamicProxy/Addons/InterfaceProxyGenerator.cs
@@ -0,0 +1,38 @@
+﻿using AspectCore.Configuration;
+using AspectCore.DynamicProxy;
+using Xunit;
+
+namespace AspectCore.Tests.DynamicProxy.Addons
+{
+    public class InterfaceProxyGenerator: DynamicProxyTestBase
+    {
+        [Fact]
+        public void CreateInterfaceProxy_Without_Impl()
+        {
+            var serviceProxy = ProxyGenerator.CreateInterfaceProxy<IMethodTestService>();
+            Assert.NotNull(serviceProxy);
+            var name = serviceProxy.GetName();
+            Assert.Equal("Your Name", name);
+        }
+
+
+        protected override void Configure(IAspectConfiguration configuration)
+        {
+            configuration.Interceptors.AddDelegate((ctx, next) => next(ctx), Predicates.ForService("IService"));
+
+            configuration.Interceptors.AddDelegate(async (ctx, next) =>
+            {
+                await next(ctx);
+                ctx.ReturnValue = "Your Name";
+            }
+            , Predicates.ForMethod("*Name"));
+        }
+
+
+    }
+
+    public interface IMethodTestService
+    {
+        string GetName();
+    }
+}
